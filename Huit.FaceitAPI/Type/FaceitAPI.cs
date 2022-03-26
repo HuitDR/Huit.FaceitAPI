47b41ff67774b4d6d336efeea4487375daeca4d8
Dmitry ROMANENKO <lady.elenaromanenko@gmail.com>
2022-03-26 18:39:30 +0700

    Matches group methods

--- Huit.FaceitAPI/Type/FaceitAPI.cs
+++ Huit.FaceitAPI/Type/FaceitAPI.cs
@@ -13,10 +13,7 @@ namespace Huit.FaceitAPI.Type
     public class APIFaceit  :   SimpleContainer<ApiBase>
     {
         protected Authorization authorization;
-        private HttpResponseMessage httpResponseMessage;
-        private string ResponseContent;
-        private JsonDeserializer jsonDeserializer = new JsonDeserializer();
-        IHttpClient httpClient = new Client();
+
         public APIFaceit(Authorization _authorization)
         {
             authorization = _authorization;
@@ -31,20 +28,7 @@ namespace Huit.FaceitAPI.Type
             return (T)instance;
         }
 
-        // matches
-        // /matches/{match_id}/stats
-        public MatchStats GetMatchStatistic(string MatchID)
-        {
-            httpResponseMessage = httpClient.SendRequest("https://open.faceit.com/data/v4/matches/"+MatchID+"/stats", authorization);
-            ResponseContent = httpResponseMessage.Content.ReadAsStringAsync().Result;
-            if (ResponseContent != "Unauthorized" &&
-                ResponseContent != "Forbidden" &&
-                ResponseContent != "Not Found")
-            {
-                return jsonDeserializer.Deserialize<MatchStats>(ResponseContent);
-            }
-            return null;
-        }
+        
         
 
     }
--- Huit.FaceitAPI/Type/Matches.cs
+++ Huit.FaceitAPI/Type/Matches.cs
@@ -3,6 +3,7 @@ using Huit.FaceitAPI.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
@@ -11,12 +12,41 @@ namespace Huit.FaceitAPI.Type
 {
     public class Matches : ApiBase, IMatches
     {
+        private HttpResponseMessage httpResponseMessage;
+        private string ResponseContent;
+        private JsonDeserializer jsonDeserializer = new JsonDeserializer();
+        IHttpClient httpClient = new Client();
+        protected Authorization authorization;
         public Matches(Authorization authorization) : base (authorization)
         {
-
+            this.authorization = authorization;
         }
-        public MatchDetails GetMatch(string match_id) => this.Get<MatchDetails>("https://open.faceit.com/data/v4/matches/" + match_id);
+        public MatchDetails GetMatch(string MatchID)
+        {
+            httpResponseMessage = httpClient.SendRequest("https://open.faceit.com/data/v4/matches/" + MatchID, authorization);
+            ResponseContent = httpResponseMessage.Content.ReadAsStringAsync().Result;
+            if (ResponseContent != "Unauthorized" &&
+                ResponseContent != "Forbidden" &&
+                ResponseContent != "Not Found")
+            {
+                return jsonDeserializer.Deserialize<MatchDetails>(ResponseContent);
+            }
+            return null;
+        }
 
-        public MatchStats GetMatchStatistics(string match_id) => this.Get<MatchStats>("https://open.faceit.com/data/v4/matches/" + match_id + "/stats");
+        // matches
+        // /matches/{match_id}/stats
+        public MatchStats GetMatchStatistics(string MatchID)
+        {
+            httpResponseMessage = httpClient.SendRequest("https://open.faceit.com/data/v4/matches/" + MatchID + "/stats", authorization);
+            ResponseContent = httpResponseMessage.Content.ReadAsStringAsync().Result;
+            if (ResponseContent != "Unauthorized" &&
+                ResponseContent != "Forbidden" &&
+                ResponseContent != "Not Found")
+            {
+                return jsonDeserializer.Deserialize<MatchStats>(ResponseContent);
+            }
+            return null;
+        }
     }
 }
